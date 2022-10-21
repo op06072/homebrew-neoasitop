193b30f273fca418521fade236bf96296231d49c
op06072 <op06072@gmail.com>
2022-10-21 13:09:01 +0900

    fix the bug that use test data

--- Formula/neoasitop.rb
+++ Formula/neoasitop.rb
@@ -2,8 +2,8 @@ class Neoasitop < Formula
   desc "Sudoless performance monitoring CLI tool for Apple Silicon written in Swift"
   homepage "https://github.com/op06072/NeoAsitop/download/neoasitop.tar.gz"
   url "https://github.com/op06072/NeoAsitop/releases/latest/download/neoasitop.tar.gz"
-  sha256 "3a32360876449772da343181687ea1b25941dda9ea5f5bc23361ba16b57b2e16"
-  version "2.1"
+  sha256 "22a29e0fdcc22885ea859cebb4cd7244aacb39b378e024eefd0d6b5a35026837"
+  version "2.2"
 
   def install
     bin.install "neoasitop"
