dd8dc090304c8ec90bb82ec2440fdba04de58b18
op06072 <op06072@gmail.com>
2022-11-03 11:18:32 +0900

    version update(2.4)

--- Formula/neoasitop.rb
+++ Formula/neoasitop.rb
@@ -3,7 +3,7 @@ class Neoasitop < Formula
   homepage "https://github.com/op06072/NeoAsitop/download/neoasitop.tar.gz"
   url "https://github.com/op06072/NeoAsitop/releases/latest/download/neoasitop.tar.gz"
   sha256 "a0f52feed2a22450a5df7798ac3fd06c245c5d6ffa73fe8ebab6636cf78283cd"
-  version "2.3"
+  version "2.4"
 
   def install
     bin.install "neoasitop"
