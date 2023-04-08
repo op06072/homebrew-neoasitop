2eea2238e35b933cb8ff51fbd5be3e1b14dbbdca
Eom Sehwan <op06072@gmail.com>
2023-04-08 19:04:57 +0900

    version update(2.7)

--- Formula/neoasitop.rb
+++ Formula/neoasitop.rb
@@ -1,10 +1,11 @@
 class Neoasitop < Formula
   desc "Sudoless performance monitoring CLI tool for Apple Silicon written in Swift"
-  homepage "https://github.com/op06072/NeoAsitop/download/neoasitop.tar.gz"
+  homepage "https://github.com/op06072/NeoAsitop"
   url "https://github.com/op06072/NeoAsitop/releases/latest/download/neoasitop.tar.gz"
-  sha256 "147e43aaa37fb90f377fe1139f6567f0113132b441dfa6ea5f608a49e7d9183f"
-  version "2.6"
-
+  sha256 "099404bc425eb6f9d23fb36711f3b783a7bfcf3f3b50109fad47edad65d9caaa"
+  version "2.7"
+  depends_on arch: :arm64
+  depends_on macos: :arm64_big_sur
   def install
     bin.install "neoasitop"
   end
